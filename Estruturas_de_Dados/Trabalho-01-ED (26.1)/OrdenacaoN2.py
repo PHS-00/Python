d064bf886ba26e7cfac80b199b398842e3b6fbab
Pedro Henrique da Silva <pedro.silva.437@ufrn.edu.br>
2026-05-18 15:27:17 +0000

    Codigos que faltava

--- Estruturas_de_Dados/Trabalho-01-ED (26.1)/OrdenacaoN2.py
+++ Estruturas_de_Dados/Trabalho-01-ED (26.1)/OrdenacaoN2.py
@@ -3,14 +3,40 @@ Arquivo Python para inserção de códigos de algoritmos de ordenação O(n^2)
 autor: Arthur Souza
 """
 
+def bubbleSortIterativo(lista):
+    n = len(lista)
+
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+
+    return lista
+
+
+def bubbleSortRecursivo(lista, n=None):
+    if n is None:
+        n = len(lista)
+
+    if n == 1:
+        return lista
+
+    for i in range(n - 1):
+        if lista[i] > lista[i + 1]:
+            lista[i], lista[i + 1] = lista[i + 1], lista[i]
+
+    return bubbleSortRecursivo(lista, n - 1)
+
+
 def ordenarIterativoN2(lista):
     '''
     Incluir código do algoritmo de ordenação iterativo O(n^2)
     '''
-    return lista
+    return bubbleSortIterativo(lista)
+
 
 def ordenarRecursivoN2(lista):
     '''
     Incluir código do algoritmo de ordenação recursiva O(n^2)
-    '''    
-    return lista
+    '''
+    return bubbleSortRecursivo(lista)
--- Estruturas_de_Dados/Trabalho-01-ED (26.1)/OrdenacaoNLog.py
+++ Estruturas_de_Dados/Trabalho-01-ED (26.1)/OrdenacaoNLog.py
@@ -3,14 +3,59 @@ Arquivo Python para inserção de códigos de algoritmos de ordenação O(n*Log(
 autor: Arthur Souza
 """
 
+def merge(esquerda, direita):
+    resultado = []
+    i = j = 0
+
+    while i < len(esquerda) and j < len(direita):
+        if esquerda[i] < direita[j]:
+            resultado.append(esquerda[i])
+            i += 1
+        else:
+            resultado.append(direita[j])
+            j += 1
+
+    resultado.extend(esquerda[i:])
+    resultado.extend(direita[j:])
+
+    return resultado
+
+
+def mergeSortRecursivo(lista):
+    if len(lista) <= 1:
+        return lista
+
+    meio = len(lista) // 2
+    esquerda = mergeSortRecursivo(lista[:meio])
+    direita = mergeSortRecursivo(lista[meio:])
+
+    return merge(esquerda, direita)
+
+
+def mergeSortIterativo(lista):
+    largura = 1
+    n = len(lista)
+
+    while largura < n:
+        for i in range(0, n, 2 * largura):
+            esquerda = lista[i:i+largura]
+            direita = lista[i+largura:i+2*largura]
+            lista[i:i+2*largura] = merge(esquerda, direita)
+
+        largura *= 2
+
+    return lista
+
+
 def ordenarIterativoNLog(lista):
     '''
     Incluir código do algoritmo de ordenação iterativo O(n*Log(n))
-    '''    
-    return lista
+    '''
+    return mergeSortIterativo(lista)
+
 
 def ordenarRecursivoNLog(lista):
     '''
     Incluir código do algoritmo de ordenação recursiva O(n*Log(n))
-    '''    
-    return lista 
+    '''
+    return mergeSortRecursivo(lista)
